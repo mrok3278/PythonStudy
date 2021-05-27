3feccb9611e559cd5bdb75ad65a4fd7b6601195f
ROK <mrok3278@naver.com>
2021-05-27 21:42:06 +0900

    0527

--- CodingTest.py
+++ CodingTest.py
@@ -1,47 +1,59 @@
 
 import random
+import math
 
-# 압축 변수 만들기
+def compressText(sText):
+    lResult = []
+    
+    for iStep in range(1, math.floor(len(sText) / 2) + 1):
+        
+        sResult = ''
+        iCount = 1
+        
+        for iIndex in range(0, len(sText), iStep):
+            
+            iSIndex = iIndex
+            iEIndex = iSIndex + iStep
+            sCurrText = sText[iSIndex:iEIndex]
+            
+            iSIndex = iIndex + iStep
+            iEIndex = iSIndex + iStep
+            sNextText = sText[iSIndex:iEIndex]
+            
+            if sCurrText == sNextText:
+                iCount += 1
+            else:
+                sResult = "%s%d%s" %(sResult,iCount,sCurrText)
+                iCount = 1
+        
+        lResult.append(sResult.replace('1',''))
+    
+    return lResult 
+
 sText=""
 for iLoop in range(1,101,1):
-   sText += chr(random.randint(97,122))
-
-
-lResult =[]
-# 1자리수 압축
-sText = 'abbcccddddeeeee' #a2b3c4d5e'
-sText += ':'
-
-sResult = ''
-iCount = 0
-
-for i in range(len(sText) - 1):
-    print(i)
-    iCount += 1
     
-    if sText[i] != sText[i+1]:
-        sResult = "%s%s%d" %(sResult,sText[i],iCount)
-        iCount = 0
+#    sText += chr(random.randint(97,122))
+   sText += chr(random.randint(97,98))
+   
+# lResult = compressText(sText)
+lResult = compressText("abcdabcd")
+iMinIndex = 0
+iMaxIndex = 0
+iMinLength = len(sText)
+iMaxLength = 0
 
-
-lResult.append(sResult.replace('1',''))
-print(lResult)
-
-# 2자리수 압축
-sText = 'abbcbc' #a2b3c4d5e'
-sText += ':'
-
-sResult = ''
-iCount = 0
-
-for i in range(len(sText) - 1):
-    print(i)
-    iCount += 1
+for iIndex, sText in enumerate(lResult, 0):
     
-    if sText[i] != sText[i+1]:
-        sResult = "%s%s%d" %(sResult,sText[i],iCount)
-        iCount = 0
+    if iMinLength > len(sText):
+        iMinLength = len(sText)
+        iMinIndex = iIndex
+    
+    if iMaxLength <= len(sText):
+        iMaxLength = len(sText)
+        iMaxIndex = iIndex
+    
+    # print(iIndex+1, sText)        
 
-
-lResult.append(sResult.replace('1',''))
-print(lResult)
+print("Min = %d, %d, %.2f, %s" %(iMinIndex + 1, iMinLength, (len(sText)/iMinLength*100),lResult[iMinIndex]))
+print("Max = %d, %d, %.2f, %s" %(iMaxIndex + 1, iMaxLength, (len(sText)/iMaxLength*100) ,lResult[iMaxIndex]))
---
+++ Day13-0527.py
@@ -0,0 +1,71 @@
+# Day13-0527.py
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome('Resource/chromedriver.exe')
+driver.get('https://map.kakao.com/')
+
+img_notice = driver.find_element_by_xpath('/html/body/div[10]/div/div/div/strong')
+img_notice.click()
+time.sleep(1)
+
+btn_login = driver.find_element_by_xpath('//*[@id="btnLogin"]')
+btn_login.click()
+time.sleep(1)
+
+btn_QRLogin = driver.find_element_by_xpath('//*[@id="move_to_qr"]')
+btn_QRLogin.click()
+time.sleep(1)
+
+input('Press enter key after login.')
+
+file = open("Resource/PlaceList.txt", "r", encoding="utf-8")
+sLines = file.readlines()
+file.close()
+
+for sLine in sLines:
+    sLine = sLine.strip()
+    sPlace = sLine.split(',')[0]
+    sType = sLine.split(',')[1]
+    
+    if not sPlace:
+        continue
+    
+    # 장소 검색
+    input_search = driver.find_element_by_xpath('//*[@id="search.keyword.query"]')
+    input_search.clear()
+    input_search.send_keys(sPlace, Keys.ENTER)
+    time.sleep(1)
+    
+    # 이미 등록된 장소
+    marked = driver.find_element_by_xpath('//*[@id="info.search.place.list"]/li[1]/div[1]/a[1]/span[2]')
+    if marked.text:
+        continue
+    
+    # 결과 중 첫번째 장소 북마크 클릭
+    a_first_place = driver.find_element_by_xpath('//*[@id="info.search.place.list"]/li[1]/div[1]/a[1]')
+    a_first_place.click()
+    time.sleep(1)
+    
+    # 첫번째 폴더 클릭
+    a_first_folder = driver.find_element_by_xpath('/html/body/div[20]/div[2]/div[2]/ul/li[2]/a')
+    a_first_folder.click()
+    time.sleep(1)
+    
+    # 색상선택
+    favoriteColor = driver.find_element_by_xpath(f'//*[@id="favoriteColor{sType}"]')
+    favoriteColor.click()
+    
+    # 완료 버튼 클릭
+    btn_finish = driver.find_element_by_xpath('/html/body/div[20]/div[3]/form/fieldset/div[3]/button')
+    btn_finish.click()
+    time.sleep(1)
+    
+    # 완료 후 팝업창 닫기 클릭
+    a_close = driver.find_element_by_xpath('/html/body/div[20]/div[7]/div[2]/a')
+    a_close.click()
+    time.sleep(1)   # 1초 대기
+  
+
